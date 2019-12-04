2f7f459b4db69ff56fe214f876b72cd59f23ad71
Nathan Chua <nchua235@mtroyal.ca>
2019-12-03 18:51:49 -0700

    finalized notMNIST model for p2

--- Part2/notMNISTStarter.py
+++ Part2/notMNISTStarter.py
@@ -19,15 +19,24 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 print("--Make model--")
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(10, activation='sigmoid')
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(1024, activation='relu'),
+  tf.keras.layers.Dropout(0.1),
+  tf.keras.layers.Dense(100, activation='sigmoid'),
+  tf.keras.layers.Dropout(0.1),
+  tf.keras.layers.Dense(10, activation='softmax'),
 ])
-model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 print("--Fit model--")
-model.fit(x_train, y_train, epochs=1, verbose=2)
+model.fit(x_train, y_train, epochs=8, verbose=2)
 
-print("--Evaluate model--")
-model_loss, model_acc = model.evaluate(x_test, y_test, verbose=2)
+print("--Evaluate Train model--")
+model_loss, model_acc = model.evaluate(x_train,  y_train, verbose=2)
 print(f"Model Loss:    {model_loss:.2f}")
-print(f"Model Accuray: {model_acc * 100:.1f}%")
+print(f"Model Accuracy: {model_acc*100:.1f}%")
+
+print("--Evaluate Test model--")
+model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
+print(f"Model Loss:    {model_loss:.2f}")
+print(f"Model Accuracy: {model_acc*100:.1f}%")
